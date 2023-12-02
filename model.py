c6dfb35128409254a4d4529d23e8d54f14813b98
Youssef Aboelwafa <96186143+YoussefAboelwafa@users.noreply.github.com>
2023-12-02 16:11:45 +0200

    Model

---
+++ model.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+import pickle
+
+# Load the data
+data = pd.read_csv('iris.csv')
+
+# Split the data into X and y
+X = data.drop('Class', axis=1)
+y = data['Class']
+
+# Split the data into train and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=5)
+
+# Scale the data
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+# Train the model
+model = RandomForestClassifier()
+model.fit(X_train, y_train)
+
+# Save the model
+pickle.dump(model, open("model.pkl", "wb"))
